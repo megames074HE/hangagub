2eec7a5d2deed3d178fea1dbcf11629063fbdfc7
megames074HE <mees.vandenbos@elev.ga.ntig.se>
2026-01-23 09:38:59 +0100

    ny del som hittar alla bokstaver i orden som man mäter in.

--- app.py
+++ app.py
@@ -4,6 +4,27 @@ import json
 
 with open('words.json', encoding='UTF-8') as f:
     wordlist = json.load(f)
-    word = choice(wordlist)
-    print(word)
+    #word = choice(wordlist['ord'])
+    word = wordlist[1]
 
+    word_playing = list(word)
+
+    print(word_playing)
+
+    guessed_letters = ["_"] * len(word)
+
+    print(guessed_letters)
+
+    player_guess = ""
+    
+    player_guess_letter = list(player_guess)
+
+    for i, letter in enumerate(word_playing):
+
+        if letter in player_guess_letter:
+            print(f"hittade: {letter}, plats: {i}")
+            guessed_letters[i] = letter
+    
+            
+
+print(guessed_letters)
