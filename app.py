8890d3664b02875aa67975a69b885fc4087b1769
megames074HE <mees.vandenbos@elev.ga.ntig.se>
2026-01-30 09:37:39 +0100

    Gjorde det lite snyggare. Basen i spelet funkar. måste bara adda score. Gjorde att terminal cleara när man får en ny meddelande av spelet så det inte kommer jättemycket gval. Nästa gång blir det manga lista

--- app.py
+++ app.py
@@ -1,28 +1,55 @@
 from random import choice
+import os
 import json
+import time
 
 play = True
 
+
 wrong_letters_list = []
+wrong_guess_count = 0
+os.system('cls' if os.name == 'nt' else 'clear')
 
-with open('words.json', encoding='UTF-8') as f:
-    wordlist = json.load(f)
-    #word = choice(wordlist['ord'])
-    word = wordlist[1]
+def word_selection():
+    with open('words.json', encoding='UTF-8') as f:
+        wordlist = json.load(f)
+        word = choice(wordlist)
+        #word = wordlist[1]
 
-    word_playing = list(word)
+        word_playing = list(word)
 
-    print(word_playing)
+        guessed_word = ["_"] * len(word)
+    return(word_playing, guessed_word, word)
 
-    guessed_word = ["_"] * len(word)
+word_playing, guessed_word, word = word_selection()
 
 while play:
 
-    if guessed_word == word_playing:
-        print("You guessed the word")
+
+    print(wrong_guess_count)
+    if wrong_guess_count == 10:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(f"Jadu nu dog du och hänger du. Ordet va: {word}")
+        print("Score: <PLACEHOLDERSCORE>")
         play_again_input = input("Vill du spela igen? Y/N ").lower()
         if play_again_input == "y":
+            wrong_letters_list = []
+            wrong_guess_count = 0
             play = True
+            word_playing, guessed_word, word = word_selection()
+        else:
+            play = False
+            break
+
+    if guessed_word == word_playing:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Du gissade ordet rätt!!")
+        play_again_input = input("Vill du spela igen? Y/N ").lower()
+        if play_again_input == "y":
+            wrong_letters_list = []
+            wrong_guess_count = 0
+            play = True
+            word_playing, guessed_word, word = word_selection()
         else:
             play = False
             break
@@ -30,26 +57,37 @@ while play:
 
 
     else:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(guessed_word)
         print(f"Fel gissade bokstaver: {wrong_letters_list}")
+        print(f"Du har {10 - wrong_guess_count} försök kvar!")
         player_guess = input("Gissa en Bokstav: ").lower()
+        if len(player_guess) > 1:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print('\nNej du bara en bokstav')
+            time.sleep(1)
+        else:
+            if player_guess in wrong_letters_list:
+                os.system('cls' if os.name == 'nt' else 'clear')
+                print("\nDen här bokstaven har du redan gissat fel!")
+                time.sleep(1)
 
-        if player_guess in wrong_letters_list:
-            print("\nDen här bokstaven har du redan gissat fel!")
-            
-        
-        try:
-            word_playing.index(player_guess)
-            for i, letter in enumerate(word_playing):
-
-                if player_guess == letter:
-
-                    guessed_word[i] = player_guess
                 
+            else:
+                try:
+                    word_playing.index(player_guess)
+                    for i, letter in enumerate(word_playing):
 
-        except ValueError:
+                        if player_guess == letter:
 
-            print("\nDen där bokstaven var FEL!\n")
-            wrong_letters_list.append(player_guess)
+                            guessed_word[i] = player_guess
+                        
+                except ValueError:
+                    os.system('cls' if os.name == 'nt' else 'clear')
+                    wrong_guess_count += 1
+                    print("\nDen där bokstaven var FEL!\n")
+                    wrong_letters_list.append(player_guess)
+                    time.sleep(1)
 
 
             
@@ -62,6 +100,5 @@ while play:
 
     
 
-    print(guessed_word)
 
 
